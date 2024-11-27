25e77fa32462533a6f7235be3a11aaebfbe1bbd6
moussa22 <cheickoumar6@yahoo.fr>
2024-11-26 19:48:27 -0500

    second commit

--- src/main/java/ch/tect/sa/SaController.java
+++ src/main/java/ch/tect/sa/SaController.java
@@ -6,14 +6,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
-
 @RestController
 @RequestMapping(path="sa" ,produces= MediaType.APPLICATION_JSON_VALUE)
 public class SaController {
-
     @GetMapping(value = "")
     public List<Object> search(){
-
         return List.of(new Sa(1,"Belle Formation",1));
 
     }
